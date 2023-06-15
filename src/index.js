9e2e687507b04fbb3678b6a2cf1c7165b007edac
Rohan Thakur <65813683+ShinchanThakur@users.noreply.github.com>
2023-06-15 16:58:55 +0530

    Merge Setup Connections to Master (#1)
    * Setup express server
    
    * Setup Websocket server

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket Connection');
+
+    socket.on('join', () => {
+        console.log('Welcome');
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+});
