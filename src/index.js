4e77a30f5c0cda9f094929fcddf8380fa039dd3c
Rohan Thakur <rohanthakurarmy@gmail.com>
2023-06-22 04:53:21 +0530

    Sending room data on join and disconnect

--- src/index.js
+++ src/index.js
@@ -27,6 +27,10 @@ io.on('connection', (socket) => {
         //Instead of using room, we use user.room, because it has been cleaned
         socket.emit('message', generateMessage('Admin', 'Welcome!'));
         socket.broadcast.to(room).emit('message', generateMessage('Admin', `${user.username} has joined!`));
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        });
         callback();
     });
 
@@ -51,6 +55,10 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id);
         if (user) {
             io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left`));
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            });
         }
     });
 });
